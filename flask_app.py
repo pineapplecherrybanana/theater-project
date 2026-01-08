69c68c802d07045eb8ed7b08cd40de1c8596aef2
pineapplecherrybanana <stephanie.sieger@mng.ch>
2026-01-08 10:07:27 +0000

    debug

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, flash
 from dotenv import load_dotenv
 import os
 import git
