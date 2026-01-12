c10be404c811ff11e5b8a6bebdf761d4984ed438
pineapplecherrybanana <stephanie.sieger@mng.ch>
2026-01-12 20:13:03 +0000

    organise code

--- flask_app.py
+++ flask_app.py
@@ -201,9 +201,6 @@ def actors():
 def scenes():
     # GET
     if request.method == "GET":
-        scenes = db_read("SELECT id, scene_name FROM scenes WHERE user_id=%s ORDER BY scene_name", (current_user.id,))
-        roles = db_read("SELECT id, role_name FROM roles WHERE user_id=%s ORDER BY role_name", (current_user.id,))
-
         query = """
             SELECT s.id, s.scene_name, GROUP_CONCAT(r.role_name SEPARATOR ', ') as roles_list
             FROM scenes s
@@ -214,6 +211,7 @@ def scenes():
             ORDER BY s.scene_name
         """
         scenes = db_read(query, (current_user.id,))
+        roles = db_read("SELECT id, role_name FROM roles WHERE user_id=%s ORDER BY role_name", (current_user.id,))
 
         return render_template("scenes.html", scenes=scenes, roles=roles)
     # POST
